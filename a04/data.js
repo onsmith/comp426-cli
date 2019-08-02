02618d05bca881e64bc67bc23a1062ae7799af73
Aaron J. Smith <onsmith@cs.unc.edu>
2019-08-02 15:17:09 -0700

    Possible direction for a04/a05: hero data

--- a04/data.js
+++ a04/data.js
@@ -2,41 +2,98 @@
  * Course: COMP 426
  * Assignment: a04
  *
- * This file defines the data that will be rendered to the page.
+ * This file defines data to be rendered to the page.
  */
 
 
 /**
- * The data to be displayed in a <table> on the page
+ * Represents a list of heroic users
  */
-const animalData = [{
-    name: "Tweety",
-    type: "bird",
-    color: "yellow",
-    age: 4,
-    birthday: new Date(1956, 1, 9),
+const heroData = [{
+    id: 1,
+    first: "Tony",
+    last: "Stark",
+    hero: "Iron Man",
+    link: "icons/tonystark.png",
+    background: "#f39c11",
 }, {
-    name: "Elmer Fudd",
-    type: "hunter",
-    color: "brown",
-    age: 46,
-    birthday: new Date(1958, 7, 21),
+    id: 2,
+    first: "Scott",
+    last: "Lang",
+    hero: "Ant-Man",
+    link: "icons/antman.png",
+    background: "#f1c40d",
 }, {
-    name: "Bugs",
-    type: "bunny",
-    color: "gray",
-    age: 29,
-    birthday: new Date(1956, 10, 6),
+    id: 3,
+    first: "Natasha",
+    last: "Romanova",
+    hero: "Black Widow",
+    link: "icons/blackwidow.png",
+    background: "#323232",
 }, {
-    name: "Roadrunner",
-    type: "bird",
-    color: "blue",
-    age: 18,
-    birthday: new Date(1961, 4, 22),
+    id: 4,
+    first: "Steve",
+    last: "Rogers",
+    hero: "Captain America",
+    link: "icons/captainamerica.png",
+    background: "#c1382b",
 }, {
-    name: "Jerry",
-    type: "mouse",
-    color: "brown",
-    age: 13,
-    birthday: new Date(1964, 6, 6),
+    id: 5,
+    first: "Bruce",
+    last: "Banner",
+    hero: "The Hulk",
+    link: "icons/hulk.png",
+    background: "#55a148",
+}, {
+    id: 6,
+    first: "Thor",
+    last: "Odinson",
+    hero: "Thor",
+    link: "icons/thor.png",
+    background: "#505050",
+}, {
+    id: 7,
+    first: "Nick",
+    last: "Fury",
+    hero: "Nick Fury",
+    link: "icons/nickfury.png",
+    background: "#e67e22",
+}, {
+    id: 8,
+    first: "Clint",
+    last: "Barton",
+    hero: "Hawkeye",
+    link: "icons/hawkeye.png",
+    background: "#8d43ac",
+}];
+
+
+/**
+ * Represents a list of heroic messages
+ */
+const messageData = [{
+    id: 1,
+    author: 5,
+    text: "HULK SMASH!",
+    postedAt: new Date(2019, 3, 5),
+}, {
+    id: 2,
+    author: 7,
+    text: "I don't think I'd do that if I were you.",
+    postedAt: new Date(2019, 6, 12),
+}, {
+    id: 2,
+    author: 6,
+    text: "My hammer is big.",
+    postedAt: new Date(2019, 4, 12),
+}, {
+    id: 2,
+    author: 4,
+    text: "Come on guys, let's work together as a team!",
+    postedAt: new Date(2019, 8, 31),
+}, {
+    id: 1,
+    author: 4,
+    text: "I work alone.",
+    postedAt: new Date(2019, 9, 1),
 }];
--- a04/render.js
+++ a04/render.js
@@ -3,32 +3,61 @@
  * Assignment: a04
  * Author: <type your name here>
  *
- * This script uses jQuery to render the data stored in the global variable
- *     "animalData" as HTML and insert it into the DOM
+ * This script uses jQuery to build an HTML page with content taken from the
+ * data defined in data.js.
  */
 
 
 
 /**
- * Given a list of animal objects, this function generates a <table> with
- *     a header row followed by a row for each animal
- * @param animals  An array of animal objects (see data.js)
+ * Given a list of hero objects, this function generates a <table> with
+ *     a header row followed by a row for each hero
+ * @param heroes  An array of hero objects (see data.js)
  */
-const renderAnimalTable = function(animals) {
+const renderHeroTable = function(heroes) {
     // TODO: Implement this function
 
-    // TODO: Should return a html elements including a <table> html element
+    // TODO: Should return html elements including a <table> html element
 };
 
 
 
 /**
- * Given an animal object, this function generates a <form> which allows the
+ * Given a hero object, this function generates a "card" showing the hero's
+ *     name, information, and colors.
+ * @param heroes  An array of hero objects (see data.js)
+ */
+const renderHeroCard = function(heroes) {
+    // TODO: Implement this function
+
+    // TODO: Should return html elements representing the hero's card
+};
+
+
+
+/**
+ * Given a message object, this function generates a <form> which allows the
  *     user to edit the fields of the animal. The form inputs should be
  *     pre-populated with the initial values of the animal.
- * @param  animal  A single animal object (see data.js)
+ * @param  message  A single message object (see data.js)
+ * @param  heroes  A list of all hero objects (see data.js)
  */
-const renderAnimalEditForm = function(animal) {
+const renderAnimalEditForm = function(message, heroes) {
+    // TODO: Implement this function
+
+    // TODO: Should return html elements including a <form> element
+};
+
+
+
+/**
+ * Given a message object, this function generates a <form> which allows the
+ *     user to edit the fields of the animal. The form inputs should be
+ *     pre-populated with the initial values of the animal.
+ * @param  message  A single message object (see data.js)
+ * @param  heroes  A list of all hero objects (see data.js)
+ */
+const renderAnimalEditForm = function(message, heroes) {
     // TODO: Implement this function
 
     // TODO: Should return html elements including a <form> element
@@ -43,8 +72,8 @@ $(function() {
     // Grab a jQuery reference to the root HTML element
     const $root = $('#root');
 
-    // Render the animal table
-    // TODO: Call renderAnimalTable() and add the result to $root
+    // Render the hero table
+    // TODO: Call renderHeroTable() and add the result to $root
 
     // Pick an animal from the list at random
     const randomAnimal = animalData[Math.floor(Math.random() * animalData.length)];
